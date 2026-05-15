fdb5ce9c5a95a7132bf78e0efc19522921f6df8b
Giovani Ouro Preto <giovaniouropreto@gmail.com>
2026-05-14 21:47:27 -0300

    headless fix for actions

--- features/hooks/hooks.js
+++ features/hooks/hooks.js
@@ -3,7 +3,7 @@ const { chromium } = require('@playwright/test');
 const { LoginPage } = require('../../page-objects/LoginPage');
 
 Before(async function () {
-    this.browser = await chromium.launch({headless:false});
+    this.browser = await chromium.launch({headless:true});
     this.context = await this.browser.newContext();
     this.page = await this.context.newPage();
     this.loginPage = new LoginPage(this.page);
